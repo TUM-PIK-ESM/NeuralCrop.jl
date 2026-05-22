dc6b5ab79227e6d3c1d6e3b12ba062d3e74133ab
yunan-l <yunan2.lin@gmail.com>
2026-05-22 16:00:39 +0200

    Fix soil water function

--- src/hybrid/soil_water.jl
+++ src/hybrid/soil_water.jl
@@ -17,11 +17,11 @@ function soil_water!(nn_model,
 
     #compute soil water content
     if irrigation
+        soil.swc = soil.wfc .* soil.layer_depth
+    else
         # soil moisture is set to field capacity every day, without constraints on water availability
         input = vcat(mean(soil.temp, dims = 1)/30, reshape(sw_n, (1, :)), reshape(lw_n, (1, :)), mean(soil.Ks, dims = 1)/20)
         soil.swc = neural_moisture(nn_model, soil.swc, ps, st, input, soil.layer_depth, soil.perc, crop.trans_layer, soil.evap)
         # # soil.swc = neural_moisture(nn_model, soil.swc, ps, st, input, soil.layer_depth, soil.perc, crop.trans_layer)
-    else
-        soil.swc = soil.wfc .* soil.layer_depth
     end
 end
