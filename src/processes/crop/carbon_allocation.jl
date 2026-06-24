15ff3232e3623c509cf17d5ba6e6297ea0d79c96
yunan-l <yunan2.lin@gmail.com>
2026-06-24 19:30:57 +0200

    Fix biomass calculation

--- src/processes/crop/carbon_allocation.jl
+++ src/processes/crop/carbon_allocation.jl
@@ -154,6 +154,8 @@ end
         crop_rootc[cell] = zero(T)
         crop_poolc[cell] = zero(T)
         crop_stoc[cell] = zero(T)
+        crop_npp[cell] = zero(T)
+        crop_biomass[cell] = zero(T)
         crop_vscal_sum[cell] = zero(T)
         crop_ndf[cell] = zero(T)
     end
